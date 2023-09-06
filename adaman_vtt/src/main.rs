9b5bf4675388c4b74c1770cb30f7fe375493dd25
Cade Kelly <295cade1@gmail.com>
2023-09-06 16:52:21 -0400

    Successfully pulling images from the internet!

--- adaman_vtt/src/main.rs
+++ adaman_vtt/src/main.rs
@@ -1,20 +1,23 @@
 
 use bevy::prelude::*;
-//use bevy_web_asset::WebAssetPlugin;
+use bevy_remote_asset::RemoteAssetPlugin;
 //use bevy_matchbox::prelude::*;
 
 mod startup;
 
 fn main() {
-  App::new()
-      .add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-          fit_canvas_to_parent: true,
-          prevent_default_event_handling: false,
+  App::new() 
+      .add_plugins(RemoteAssetPlugin)
+      .add_plugins(
+        DefaultPlugins.set(WindowPlugin {
+          primary_window: Some(Window {
+            fit_canvas_to_parent: true,
+            prevent_default_event_handling: false,
+            ..default()
+        }),
           ..default()
-      }),
-        ..default()
-      }))
+        })
+      )
       .add_plugins(startup::GameStartPlugin)
       .run();
 }
--- adaman_vtt/src/startup.rs
+++ adaman_vtt/src/startup.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy_web_asset::
 
 pub struct GameStartPlugin;
 
@@ -10,7 +9,43 @@ impl Plugin for GameStartPlugin {
   }
 }
 
-fn setup(mut commands: Commands) {
-  let camera_bundle = Camera3dBundle::default();
-  commands.spawn(camera_bundle);
+fn setup(mut commands: Commands,
+            mut meshes: ResMut<Assets<Mesh>>,
+            mut images: ResMut<Assets<Image>>,
+            asset_server: Res<AssetServer>,
+            mut materials: ResMut<Assets<StandardMaterial>>,
+        ) {
+    let camera_bundle = Camera3dBundle {
+        transform: Transform::from_xyz(0., 100., 0.).looking_at(Vec3::new(0., 0., 0.), Vec3::Y),
+        ..default()
+    };
+    commands.spawn(camera_bundle);
+
+    let tex = Some(asset_server.load("https://i.pinimg.com/originals/27/2d/7e/272d7e20f512f3bc24713248ce626b5d.jpg"));
+    
+
+    let bg_quad = shape::Quad {
+        size: Vec2{x: 50.,y: 50.},
+        flip: false,
+    };
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(bg_quad.into()),
+        material: materials.add(StandardMaterial{
+            base_color_texture: tex,
+          ..default()
+        }),
+        transform: Transform::from_xyz(0., 0., 0.).looking_at(Vec3::new(0., -1., 0.), Vec3::Y),
+        ..default()
+    });
+    commands.spawn(PointLightBundle {
+          point_light: PointLight {
+              intensity: 9000.0,
+              range: 100.,
+              shadows_enabled: true,
+              ..default()
+          },
+          transform: Transform::from_xyz(8.0, 16.0, 8.0),
+          ..default()
+      });
 }
